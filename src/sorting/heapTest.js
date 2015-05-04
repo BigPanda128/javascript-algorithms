66a327ef513da219393daaf3b3a26ef50b8c58c7
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-04 14:33:21 -0400

    prints out 15 random elements

--- src/sorting/heapTest.js
+++ src/sorting/heapTest.js
@@ -1,8 +1,8 @@
 var sort = require('./heapsort').heapSort;
-var nums = [40,7,41,13,19,92,51,31];
-/*for (var i = 0; i < 50; ++i) {
-	 nums[i] = Math.floor(Math.random() * 51);
-}*/
+var nums = [];
+for (var i = 0; i < 15; ++i) {
+	 nums.push(Math.floor(Math.random() * 99));
+}
 
 function dispArr(arr) {
 	 for (var i = 0; i < arr.length; ++i) {
